7251a01b7e3fb7cbf8b167775bf55c837a296f57
HZertuche <he_z_san@hotmail.com>
2026-07-19 19:22:01 -0600

    Create historical-standings.ts

---
+++ artifacts/api-server/src/lib/historical-standings.ts
@@ -0,0 +1,11 @@
+import { Match, Prediction, User } from "@workspace/db";
+
+export function calculateDailyLeaders(
+  users: User[],
+  matches: Match[],
+  predictions: Prediction[],
+) {
+
+  return [];
+
+}
